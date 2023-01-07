1963c250695d80e0e96b5af257897656fb88115c
J Jesús Villanueva Jr <jesus1843@hotmail.com>
2023-01-07 00:20:25 -0600

    database configs and user model added

---
+++ database/seeders/20230106214456-admin-user.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const crypto = require('crypto');
+
+const randomString = require('randomstring');
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up (queryInterface, Sequelize) {
+    const salt = crypto.randomBytes(16).toString('hex');
+    const passwordHash = crypto.pbkdf2Sync('QyV76FEG5G*', salt, 1000, 64, 'sha512').toString('hex');
+
+    return queryInterface.bulkInsert('Users', [{
+      email: 'jjesus1843vg@gmail.com',
+      password: passwordHash,
+      salt,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
--- server/index.js
+++ server/index.js
@@ -2,13 +2,26 @@ require('dotenv').config();
 
 const express = require('express');
 
+const { sequelize } = require('./models');
+
 
 const server = express();
 
 
-server.listen(
-    +process.env.PORT,
-    () => {
-        console.log('Server running in PORT:', process.env.PORT);
+(async function() {
+    try {
+        await sequelize.authenticate();
+
+        console.log('Database running...');
+
+        server.listen(
+            +process.env.PORT,
+            () => {
+                console.log('Server running in PORT:', process.env.PORT);
+            }
+        );
     }
-);
+    catch(err) {
+        console.log('Error connecting to database. Error:', err)
+    }
+})();
