47ea6259936d728f003024437cb0a8d454de7759
J Jesús Villanueva Jr <jesus1843@hotmail.com>
2023-01-08 14:25:58 -0600

    Street Controller finished

--- server/controllers/api/streets.js
+++ server/controllers/api/streets.js
@@ -3,28 +3,61 @@ const { Street } = sequelize.models;
 
 
 module.exports.index = async(req, res) => {
-    const streets = await Street.findAll();
-    res.json({
-        data: streets
-    });
+    try {
+        const streets = await Street.findAll();
+
+        res.json({
+            data: streets
+        });
+    }
+    catch(err) {
+        res.status(500).json({
+            error: JSON.parse(JSON.stringify(err))
+        });
+    }
 }
 
 
 module.exports.show = async(req, res) => {
     const { streetId } = req.params;
 
-    res.json({
-        data: 'street with id: '+ streetId
-    });
+    try {
+        const street = await Street.findOne({ where: { id: streetId } });
+
+        if (!street) {
+            return res.status(404).json({
+                code: 'STREET_NOT_FOUND',
+                message: `Street with ID: ${streetId} not found`
+            });
+        }
+
+        res.json({
+            data: street
+        });
+    }
+    catch(err) {
+        res.status(500).json({
+            error: JSON.parse(JSON.stringify(err))
+        });
+    }
 }
 
 
 module.exports.store = async(req, res) => {
     const { name } = req.body;
 
-    res.json({
-        data: 'create street with name: '+ name
-    });
+    try {
+        const street = await Street.create({ name });
+
+        res.status(201).json({
+            data: street
+        });
+    }
+    catch(err) {
+        res.status(500).json({
+            error: JSON.parse(JSON.stringify(err))
+        });
+    }
 }
 
 
@@ -32,16 +65,51 @@ module.exports.update = async(req, res) => {
     const { streetId } = req.params;
     const { name } = req.body;
 
-    res.json({
-        data: 'update street with id: '+ streetId + ', and name: '+ name
-    });
+    try {
+        const street = await Street.findOne({ where: { id: streetId } });
+
+        if (!street) {
+            return res.status(404).json({
+                code: 'STREET_NOT_FOUND',
+                message: `Street with ID: ${streetId} not found`
+            });
+        }
+
+        street.name = name;
+        await street.save();
+
+        res.status(204).json({
+            data: street
+        });
+    } catch(err) {
+        res.status(500).json({
+            error: JSON.parse(JSON.stringify(err))
+        });
+    }
 }
 
 
 module.exports.delete = async(req, res) => {
     const { streetId } = req.params;
 
-    res.json({
-        data: 'delete street with id: '+ streetId
-    });
+    try {
+        const street = await Street.findOne({ where: { id: streetId } });
+
+        if (!street) {
+            return res.status(404).json({
+                code: 'STREET_NOT_FOUND',
+                message: `Street with ID: ${streetId}, not found`
+            });
+        }
+
+        await street.destroy();
+
+        res.status(202).json({
+            data: street
+        });
+    } catch(err) {
+        res.status(500).json({
+            error: JSON.parse(JSON.stringify(err))
+        });
+    }
 }
